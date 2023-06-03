c0ac212b43488c34f103466106498fc7174a4a6e
Barbaro Garcia <barbarogarcia.000@gmail.com>
2023-06-03 02:48:14 -0400

    instalando dependencias(Api-Rest)django

--- src/API/Rutas.js
+++ src/API/Rutas.js
@@ -1,6 +1,12 @@
 import axios from 'axios'
 export const getRutas=()=>{
-   axios.get
+   axios.get('')
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 
 
 }
